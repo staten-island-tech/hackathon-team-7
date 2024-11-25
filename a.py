d2035f16c4d15bdc251d37fe01356a1593db2c30
Jiechi-wq <Jiechic@nycstudents.net>
2024-11-25 09:52:21 -0500

    Create a.py

---
+++ a.py
@@ -0,0 +1,27 @@
+#1. Task/Problem Description
+"We are trying to make a rhythm game"
+
+#2. Intial Approach/Code
+"Tried implenting code with chatgpt through questions and prompts"
+#3. Questions "everything was made by chatgpt"
+"Make a python code for a rhythm game"
+"make the border for where the note goes into, the score counter also doesn't work"
+"make it so the yellow line is a circle"
+"make the note border inside of the lines at the bottom of the screen"
+"make it so there is a border in between each notes/keys"
+"fix the input as it doesnt register along with the score"
+"the squares overlap each other"
+"the score doesnt get updated"
+"can you add a sound to when you miss the notes"
+"get rid of sounds"
+"Make it so the row isnt removed when a key is pressed"
+"Fix the score counter"
+"to be continued..."
+#4 ChatGPT's suggestion/code changes
+"We just formatted our questions around the problems of our code"
+#5 Reflection On Changes
+"It was helpful because it wrote complex codes that would've took longer otherwise"
+#6"Testing and Resultd"
+"Problems were still present, but a majroity of them were fixed(Minor Or Major). Problems:Input and sound"
+#7 What Did You Learn
+"I learned the complexity of chatgpt, with the codes it wrote, showing the difference between ai and us freshmen"
